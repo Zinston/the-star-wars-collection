f75f0ef5bb1c97f97207b2e9544618eb2cdf9b2d
Antoine Guenet <aguenet@gmail.com>
2018-01-17 10:35:35 +0100

    Add basic Movie class (title + trailer).

---
+++ media.py
@@ -0,0 +1,6 @@
+class Movie:
+	"""This class defines and display a movie. It takes a title and a trailer video."""
+
+	def __init__(self, title, trailer):
+		self.title = title
+		self.trailer = trailer
