ef451fce6221771ba323ad14c49a08ccbb0e442d
Antoine Guenet <aguenet@gmail.com>
2018-01-17 11:29:11 +0100

    Update to conform to pep8.

--- media.py
+++ media.py
@@ -1,7 +1,8 @@
 class Movie:
-	"""This class defines and display a movie. It takes a title and a trailer video."""
+    """This class defines and display a movie.
+       It takes a title and a trailer video."""
 
-	def __init__(self, title, trailer, poster):
-		self.title = title
-		self.trailer_youtube_url = trailer
-		self.poster_image_url = poster
+    def __init__(self, title, trailer, poster):
+        self.title = title
+        self.trailer_youtube_url = trailer
+        self.poster_image_url = poster
